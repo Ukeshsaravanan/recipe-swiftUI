40f832196f8f20afc0714cb196646d4fb3a68779
Ukesh Kumar <ukeshsaravanan@gmail.com>
2023-04-04 15:08:17 +0530

    Add RecipeTabView

---
+++ Receipe/Views/RecipeTabView.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeTabView.swift
+//  Recipe
+//
+//  Created by UKESH KUMAR on 04/04/23.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
